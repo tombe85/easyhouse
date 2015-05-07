137ea0077cc844a9d7e35e83e58e6115c5e93e9a
tombe85 <mihiguer@ucm.es>
2015-05-07 12:21:33 +0200

    Añadida base de datos a data (formato sql)

---
+++ data/sweethomesw.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 07-05-2015 a las 12:19:44
+-- Versión del servidor: 5.5.43-0ubuntu0.14.04.1
+-- Versión de PHP: 5.5.9-1ubuntu4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `sweethomesw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `board`
+--
+
+CREATE TABLE IF NOT EXISTS `board` (
+  `idboard` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `iduser` int(10) unsigned NOT NULL,
+  `content` text COLLATE utf8_spanish_ci NOT NULL,
+  `idhome` int(10) unsigned NOT NULL,
+  `date` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idboard`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `boardcomments`
+--
+
+CREATE TABLE IF NOT EXISTS `boardcomments` (
+  `idcomment` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idboard` int(11) NOT NULL,
+  `iduser` int(11) NOT NULL,
+  `comment` text COLLATE utf8_spanish_ci NOT NULL,
+  `date` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idcomment`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `expenses`
+--
+
+CREATE TABLE IF NOT EXISTS `expenses` (
+  `idexpense` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8_spanish_ci NOT NULL,
+  `idhome` int(10) unsigned NOT NULL,
+  `users` text COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idexpense`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `homes`
+--
+
+CREATE TABLE IF NOT EXISTS `homes` (
+  `idhome` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8_spanish_ci NOT NULL,
+  `numusers` int(10) unsigned NOT NULL,
+  `maxscore` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idhome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `idproduct` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8_spanish_ci NOT NULL,
+  `idhome` int(10) unsigned NOT NULL,
+  `added` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idproduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE IF NOT EXISTS `registro` (
+  `idregistro` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `iduser` int(10) unsigned NOT NULL,
+  `content` text COLLATE utf8_spanish_ci NOT NULL,
+  `idhome` int(10) unsigned NOT NULL,
+  `date` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idregistro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `idtask` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8_spanish_ci NOT NULL,
+  `period` int(11) NOT NULL,
+  `idhome` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `whenisdone` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
+  `daysactive` int(11) NOT NULL,
+  `points` int(11) NOT NULL,
+  PRIMARY KEY (`idtask`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `iduser` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text COLLATE utf8_spanish_ci NOT NULL,
+  `idhome` int(10) unsigned NOT NULL,
+  `mail` text COLLATE utf8_spanish_ci NOT NULL,
+  `photo` text COLLATE utf8_spanish_ci NOT NULL,
+  `passwd` text COLLATE utf8_spanish_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
