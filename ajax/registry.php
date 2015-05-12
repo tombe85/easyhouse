af2383de27b610fafa9b35dd029fd74457842370
migurome@ucm.es <migurome@ucm.es>
2015-05-12 13:07:54 +0200

    Completado el HOME

---
+++ ajax/registry.php
@@ -0,0 +1,31 @@
+<?php
+{
+    include_once('../backend/functions.php');
+    if(!checkLogin()){
+        echo '<h4>No has iniciado sesión. Accede al <a href="/sweethomesw/login.html" data-ajax="false">login</a></h4>';
+        exit();
+    }
+    $idhome=$_COOKIE["idhome"];
+    include('../backend/loads.php');
+    $dataArray = loadregister($idhome);
+    
+    echo '<table>';
+    foreach($dataArray as $reg){
+        echo '<tr class="rowNormal">';
+        
+        echo '<td class="rowPhoto">';
+        echo '<img src="'.$reg["rutafoto"].'" />';        
+        echo '</td>';
+        
+        echo '<td class="rowText">';
+        echo '<h3>'.$reg["content"].'</h3>';
+        echo '</td>';
+        
+        echo '<td class="rowDate"><h5>'.$reg["date"].'</h5></td>';
+        
+        echo '</tr>';
+    }
+        
+    echo '</table>';
+}
+?>
--- backend/loads.php
+++ backend/loads.php
@@ -1,4 +1,29 @@
 <?php
+    
+    function loadregister($idhome){
+        include_once('functions.php');
+        $db = connectDataBase();
+        $query='select * from registro where idhome = "'.$idhome.'"';
+        $result = $db->query($query)
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
+        
+        $arr = array();
+        $numrows=$result->num_rows;
+        for($i=0; $i < $numrows; $i++){
+            $row = $result->fetch_array();
+            $querym = 'select * from users where iduser = '.$row["iduser"];
+            $resultm = $db->query($querym)
+                or die($db->error. " en la línea ".(__LINE__-1));
+            $user = $resultm->fetch_array();
+            $arr[$i]["rutafoto"] = $user["photo"];
+            $arr[$i]["idregistro"] = $row["idregistro"];
+            $arr[$i]["content"] = $row["content"];
+            $arr[$i]["date"] = $row["date"];
+        }
+        $db->close();
+        return $arr;
+    }
+
     function loadboard($idhome){
         include_once('functions.php');
         $db = connectDataBase();
