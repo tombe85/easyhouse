e5bd1172297a14a5010a78a40dcf0f7e14601384
tombe85 <mihiguer@ucm.es>
2015-05-12 20:03:12 +0200

    Acción de borrar home Terminada

--- backend/home/delete.php
+++ backend/home/delete.php
@@ -1,6 +1,5 @@
 <?php
 {
-    echo "hola";
     include_once('../functions.php');
     if(!checklogin()){
         header('Location: /sweethomesw/login.html');
@@ -13,24 +12,24 @@
         exit();
     }
     $text = $_REQUEST["strids"];
-    $idsreg = split(" ", $text);
-    echo 'Hace el decode';
+    $idsreg = array();
+    $idsreg = split(",", $text);
     
     $db = connectDataBase();
     foreach($idsreg as $idregistro){
         echo 'entra en el for';
         $query='select * from registro where idregistro = "'.$idregistro.'"';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1).);
+            or die($db->error. " en la línea ".(__LINE__-1));
         $row = $result->fetch_array();
         $usersdeleted = $row["usersdeleted"];
         $usersdeleted = $usersdeleted . " " . $iduser;
         
         $query='update registro set usersdeleted = "'.$usersdeleted.'" where idregistro = "'.$idregistro.'"';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
+            or die($db->error. " en la línea ".(__LINE__-1));
     }
     $db->close();
-    //header('Location: /sweethomesw/home.html');
+    header('Location: /sweethomesw/home.html');
 }
 ?>
