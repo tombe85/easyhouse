0cae26ae39b7d96c0de512f592986befb4e3dc7f
ronalkinho <dquinones@ucm.es>
2015-05-14 11:50:22 +0200

    Creado removeproduct

---
+++ backend/shopping/removeproduct.php
@@ -0,0 +1,26 @@
+<?php
+  
+include_once('../functions.php');
+
+$text = $_REQUEST["strids"];
+    $idsproduct = array();
+    $idsproduct = split(",", $text);
+    
+    $db = connectDataBase();
+    foreach($idsproduct as $idproduct){
+        echo 'entra en el for';
+        $query='update products set added = false where idproduct = "'.$idproduct.'"';
+        $result = $db->query($query)
+        if($result = $db->query($query)){
+            echo "0";
+
+        }else{
+            echo "Fallo al actualizar los productos";
+        }
+        
+    }
+    $db->close();
+    header('Location: /sweethomesw/shopping/main.html');
+
+      
+?>
