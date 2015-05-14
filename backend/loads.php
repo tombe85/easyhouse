908630ccb8ee837cae9f1662bfcfb470dcb84739
ronalkinho <dquinones@ucm.es>
2015-05-14 11:47:49 +0200

    Tocado loadShoppingList

--- backend/loads.php
+++ backend/loads.php
@@ -5,7 +5,7 @@
         $db = connectDataBase();
         $query='select * from registro where idhome = "'.$idhome.'" and usersdeleted not like "%'.$iduser.'%" order by idregistro desc';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $numrows=$result->num_rows;
@@ -30,31 +30,7 @@
         $db = connectDataBase();
         $query='select * from board where idhome = "'.$idhome.'" order by idboard desc';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
-        
-        $arr = array();
-        $numrows=$result->num_rows;
-        for($i=0; $i < $numrows; $i++){
-            $row = $result->fetch_array();
-            $querym = 'select * from users where iduser = '.$row["iduser"];
-            $resultm = $db->query($querym)
-                or die($db->error. " en la línea ".(__LINE__-1));
-            $user = $resultm->fetch_array();
-            $arr[$i]["rutafoto"] = $user["photo"];
-            $arr[$i]["idboard"] = $row["idboard"];
-            $arr[$i]["message"] = $row["content"];
-            $arr[$i]["date"] = $row["date"];
-        }
-        $db->close();
-        return $arr;
-    }
-
-    function loaduserboard($idhome, $iduser){
-        include_once('functions.php');
-        $db = connectDataBase();
-        $query='select * from board where idhome = "'.$idhome.'" and iduser = "'.$iduser.'" order by idboard desc';
-        $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $numrows=$result->num_rows;
@@ -78,7 +54,7 @@
         $db = connectDataBase();
         $query='select * from board where idhome = "'.$idhome.'" and idboard = "'.$idboard.'"';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $row = $result->fetch_array();
@@ -101,7 +77,7 @@
         $db = connectDataBase();
         $query='select * from boardcomments where idboard = "'.$idboard.'"';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $numrows=$result->num_rows;
@@ -123,7 +99,7 @@
         $db = connectDataBase();
         $query='select * from tasks where idhome = "'.$idhome.'" and approved = 1';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $numrows=$result->num_rows;
@@ -144,7 +120,7 @@
                 if($daysclosed >= $row["period"]){
                     $querym = 'update tasks set active = true, activesince = "'.date("d.m.Y").'" where idtask = "'.$row["idtask"].'"';
                     $resultm = $db->query($querym)
-                        or die($db->error. " en la línea ".(__LINE__-1));
+                        or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
                     $arr[$j]["idtask"] = $row["idtask"];
                     $arr[$j]["content"] = $row["name"];
                     $arr[$j]["dayson"] = 0;
@@ -161,13 +137,15 @@
         $db = connectDataBase();
         $query='select * from products where idhome = "'.$idhome.'" and active = true and added = true';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $numrows=$result->num_rows;
         for($i=0; $i < $numrows; $i++){
             $row = $result->fetch_array();
             $arr[$i]["product"] = $row["name"];
+            $arr[$i]["idproduct"] = $row["idproduct"];
+
         }
         $db->close();
         return $arr;
@@ -178,71 +156,13 @@
         $db = connectDataBase();
         $query='select * from products where idhome = "'.$idhome.'" and active = true and added = false';
         $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
+            or die($db->error. " en la línea ".(__LINE__-1)." idhome=".$idhome);
         
         $arr = array();
         $numrows=$result->num_rows;
         for($i=0; $i < $numrows; $i++){
             $row = $result->fetch_array();
             $arr[$i]["product"] = $row["name"];
-        }
-        $db->close();
-        return $arr;
-    }
-    
-    
-    function loadusers($idhome){
-        include_once('functions.php');
-        $db = connectDataBase();
-        $query='select * from users where idhome = "'.$idhome.'"';
-        $result = $db->query($query)
-            or die($db->error. " en la línea ".(__LINE__-1));
-        
-        $arr = array();
-        $numrows=$result->num_rows;
-        for($i=0; $i < $numrows; $i++){
-            $row = $result->fetch_array();
-            $arr[$i]["photo"] = $row["photo"];
-            $arr[$i]["mail"] = $row["mail"];
-            $arr[$i]["iduser"] = $row["iduser"];
-            
-        }
-        $db->close();
-        return $arr;
-    }
-
-    function loadexpenses($idhome){
-        include_once('functions.php');
-       $db = connectDataBase();
-        
-
-        // Selecciono la lista de usuarios del idhome
-        $query =  'select * from users where idhome = "'.$idhome.'"';
-        $result2 = $db->query($query)
-            or die($db->error." en la línea ".(__LINE__-1));
-
-        // Guardo los nombres de mis usuarios en $users
-        $numusers=$result2->num_rows;
-        $users = array();
-        for($i=0;$i<$numusers;$i++){
-            $row = $result2->fetch_array();
-            $users[$i] = $row["name"];
-        }
-
-        // Selecciono los expenses del idhome
-        $query = 'select * from expenses where idhome = "'.$idhome.'"';
-        $result = $db->query($query)
-            or die($db->error." en la línea ".(__LINE__-1));
-
-        // Creo un array con el nombre del gasto, quien ha pagado o no
-        $arr = array();
-        $numrows=$result->num_rows;
-        for($i=0;$i<$numrows;$i++){
-            $row=$result->fetch_array();
-            $arr[$i]["name"] = $row["name"];
-            for($j=0;$j<$numusers;$j++){
-                    $arr[$i]["pagado"][$j] =  (strpos($row["users"],$users[$j]) !== false);
-            }
         }
         $db->close();
         return $arr;
