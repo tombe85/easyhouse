044289c2dadf488b9dc22ffb74dbfd41e15a93c2
tombe85 <mihiguer@ucm.es>
2015-05-14 17:48:33 +0200

    Shopping función comprar acabada

--- backend/shopping/removeproduct.php
+++ backend/shopping/removeproduct.php
@@ -1,26 +1,21 @@
 <?php
-  
-include_once('../functions.php');
+{
+    include_once('../functions.php');
 
-$text = $_REQUEST["strids"];
+    $text = $_REQUEST["strids"];
     $idsproduct = array();
     $idsproduct = split(" ", trim($text));
     
     $db = connectDataBase();
     foreach($idsproduct as $idproduct){
-        echo 'entra en el for';
         $query='update products set added = false where idproduct = "'.$idproduct.'"';
         $result = $db->query($query);
         if($result = $db->query($query)){
             echo "0";
-
         }else{
             echo "Fallo al actualizar los productos";
         }
-        
     }
     $db->close();
-    header('Location: /sweethomesw/shopping/main.html');
-
-      
+}
 ?>
