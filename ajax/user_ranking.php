3d492a31b8c8849fdedf6218b343a932b9686f73
migurome@ucm.es <migurome@ucm.es>
2015-05-14 12:32:41 +0200

    generado el user_ranking.php

---
+++ ajax/user_ranking.php
@@ -0,0 +1,17 @@
+<?php
+{
+    include_once('../backend/functions.php');
+    if(!checklogin()){
+        header('Location: /sweethomesw/login.html');
+    }
+    $iduser = $_COOKIE["iduser"];
+    
+    $db = connectDataBase();
+    $query='select * from users where iduser = "'.$iduser.'"';
+    $result = $db->query($query)
+        or die($db->error. " en la línea ".(__LINE__-1));
+    $row = $result->fetch_array();
+    $db->close();
+    echo '<img id="userAvatar" src="'.$row["photo"].'" />';
+}
+?>
