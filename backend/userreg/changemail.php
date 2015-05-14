efcc1289403d21db019c4f1ebf7aca2b0cc2beda
tombe85 <mihiguer@ucm.es>
2015-05-14 10:39:07 +0200

    Ación cambio de email funcionando en config y admin/config

---
+++ backend/userreg/changemail.php
@@ -0,0 +1,27 @@
+<?php
+{
+    //Comprobar passwd antigua
+    include_once('../functions.php');
+    $iduser = $_COOKIE["iduser"];
+    $mail = sha1($_REQUEST["mail"]);
+    $passwd = sha1($_REQUEST["passwd"]);
+    
+    $db = connectDataBase();
+    $query = 'select * from users where iduser = "'.$iduser.'"';
+    $result = $db->query($query)
+        or die($db->error);
+    $row = $result->fetch_array();
+    $passwddb = $row["passwd"];
+    
+    if($passwd == $passwddb){
+        $query = 'update users set mail = "'.$mail.'" where iduser = "'.$iduser.'"';
+        if($db->query($query)){
+            echo "0";
+        }else{
+            echo "Error al actualizar el correo";
+        }
+    }else{
+        echo "Contraseña incorrecta";
+    }
+}
+?>
