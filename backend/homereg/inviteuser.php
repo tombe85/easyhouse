a568dd3e4de0155020f6390de5aaf23af23640bf
tombe85 <mihiguer@ucm.es>
2015-05-19 12:51:37 +0200

    Evitar invitaciones múltiples

--- backend/homereg/inviteuser.php
+++ backend/homereg/inviteuser.php
@@ -9,9 +9,17 @@
         $code = $code . substr($caracteres,rand(0,strlen($caracteres)),1);
     }
     
-    //Agregamos a invitados
+    //Vemos que no esté invitados
     include_once('../functions.php');
     $db = connectDataBase();
+    $query = 'select * from invited where idhome = "'.$idhome.'" and mail like "'.$mail.'"';
+    $result = $db->query($query)
+        or die($db->error);
+    if($result->num_rows > 0){
+        header('Location: /sweethomesw/admin/config/users.html');
+    }
+    
+    //Agregamos a invitados
     $query = 'insert into invited (mail,code,idhome) values ("'.$mail.'","'.$code.'","'.$idhome.'")';
     $db->query($query)
         or die($db->error);
