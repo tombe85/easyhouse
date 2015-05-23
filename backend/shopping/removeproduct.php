4b73a8066a0caeb3ecd3b5f024ba59187ffde2f0
tombe85 <mihiguer@ucm.es>
2015-05-24 01:42:42 +0200

    Arreglo. Al comprar había registro en home por cada producto.

--- backend/shopping/removeproduct.php
+++ backend/shopping/removeproduct.php
@@ -16,14 +16,17 @@
     $db = connectDataBase();
     foreach($idsproduct as $idproduct){
         $query='update products set added = false where idproduct = "'.$idproduct.'"';
-        if($result = $db->query($query)){
-            $query = 'insert into registro (iduser,content,idhome,date,usersdeleted) values ('.$_COOKIE["iduser"].',"Ha hecho compra",'.$_COOKIE["idhome"].',"'.date("d.m.Y").'","")';
-            $db->query($query);
-            echo "0";
-        }else{
-            echo "No se puede actualizar la lista de productos";
+        if(!($result = $db->query($query))){
+            echo "Error al actualizar la lista de productos";
+            exit();
         }
     }
+    $query = 'insert into registro (iduser,content,idhome,date,usersdeleted) values ('.$_COOKIE["iduser"].',"Ha hecho compra",'.$_COOKIE["idhome"].',"'.date("d.m.Y").'","")';
+    
+    $db->query($query);
     $db->close();
+    
+    echo "0";
+    exit();
 }
 ?>
