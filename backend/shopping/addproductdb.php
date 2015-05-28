7b1f6bb49c0852a78725a88494ac343e5b00e6ef
tombe85 <mihiguer@ucm.es>
2015-05-28 11:24:32 +0200

    Seguridad al añadir producto (no duplicados)

--- backend/shopping/addproductdb.php
+++ backend/shopping/addproductdb.php
@@ -14,15 +14,22 @@
     $db = connectDataBase();
     $idhome = $_COOKIE["idhome"];
     //Añade producto
-    $query = 'insert into products (name,idhome,added,active) values ("'.$name.'",'.$idhome.',false,true)';
-    if($result = $db->query($query)){
-        $iduser = $_COOKIE["iduser"];
-        $content = "Ha añadido el producto: " . $name;
-        $query = 'insert into registro (iduser,content,idhome,date) values ("'.$iduser.'","'.$content.'",'.$idhome.',"'.date("d.m.Y").'")';
-        $result = $db->query($query);
-        echo "0";
+    $query = 'select * from products where idhome = "'.$idhome.'" and name like "'.$name.'"';
+    $result = $db->query($query);
+    if($result->num_rows == 0){
+        $query = 'insert into products (name,idhome,added,active) values ("'.$name.'",'.$idhome.',false,true)';
+        if($result = $db->query($query)){
+            $iduser = $_COOKIE["iduser"];
+            $content = "Ha añadido el producto: " . $name;
+            $query = 'insert into registro (iduser,content,idhome,date) values ("'.$iduser.'","'.$content.'",'.$idhome.',"'.date("d.m.Y").'")';
+            $result = $db->query($query);
+            echo "0";
+        }else{
+            echo "No se ha podido añadir la tarea";
+        }
     }else{
-        echo "No se ha podido añadir la tarea";
+        //Ya existe
+        echo "0";
     }
     $db->close();
 }
