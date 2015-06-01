1e58f1680b3a30b5c3935ac75425e6a16dfeeeb4
tombe85 <mihiguer@ucm.es>
2015-06-01 23:38:12 +0200

    Mejorado index de easyHouse general

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+
+include_once('backend/functions.php');
+if(checkLogin()){
+    updateCookies();
+    header('Location: /sweethomesw/home.html');
+}else{
     header('Location: /sweethomesw/login.html');
+}
 ?>
