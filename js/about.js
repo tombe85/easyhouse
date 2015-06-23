656a5edd29314e76f6ad86f4be6989a5feae1fbf
tombe85 <mihiguer@ucm.es>
2015-06-23 16:25:04 +0200

    Arreglo eliminar casa

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 $(document).bind('pageinit', function(){
-    $("#header").load("/sweethomesw/ajax/header.php?backbutton=true&backurl=/sweethomesw/config/index.html&info=true", function(){
-        $("#photoHeader").find("img").attr("src","/sweethomesw/img/infoselected.png");
+    $("#header").load("/ajax/header.php?backbutton=true&backurl=/config/index.html&info=true", function(){
+        $("#photoHeader").find("img").attr("src","/img/infoselected.png");
     });
     $(document).ajaxComplete(function(){
         $("body").resize();
@@ -9,17 +9,17 @@ $(document).bind('pageinit', function(){
 
 function removeHouse(){
     var sure = confirm("¿Seguro que desea eliminar la casa? Se eliminarán todos los datos y no habrá posibilidad de recuperación.");
-    if(getCoockie("admin") != true){
+    if(getCookie("admin") != true){
         alert("Para eliminar la casa debes ser adminstrador");
         
     }else{
         if(sure != false){
-            $.post('/sweethomesw/backend/homereg/removehouse.php', function(data, textStatus){
+            $.post('/backend/homereg/removehouse.php', function(data, textStatus){
                 if(data != 0){
                     alert("Error : " + data);
                 }else{
                     alert("Casa eliminada correctamente");
-                    document.location.href="/sweethomesw/backend/logout.php";
+                    document.location.href="/backend/logout.php";
                 }
             });
         }
